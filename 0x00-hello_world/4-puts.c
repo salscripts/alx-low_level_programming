62b068fec9750f04f57290ccb751654919f5cd73
Salman I <sal.idris27@gmail.com>
2022-03-13 13:16:45 -0700

    C program that displays an output using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,6 @@
  * main - prints "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
