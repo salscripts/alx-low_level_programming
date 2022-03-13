18403fdad692a48e6e0b2925baac347f5f06fbb2
Salman I <sal.idris27@gmail.com>
2022-03-13 13:32:44 -0700

    C program to display output using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art
- * Return - 0 if exited properly
+ * Return: 0 if exited properly, no-zero otherwise
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
