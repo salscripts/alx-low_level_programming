326e4f57f0921b4165b24c6886fcbb3fb5a199b7
Salman I <sal.idris27@gmail.com>
2022-03-14 07:49:19 -0700

    C program to print single digits from 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers of
+ * base 10 starting from 0
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10 ; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
