0a2a96b8f974641f6d20bf25c5756ae050238614
Salman I <sal.idris27@gmail.com>
2022-03-14 08:51:09 -0700

    C program to print numbers of base16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints the umbers of base 16
+ * main - prints the numbers of base 16
  * in lowercase
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	char c = 'a';
-	int n = 48;
+	int n;
+	char c;
 
-	while (n <= 58)
+	for (n = 48; n < 58 ; n++)
 	{
 		putchar(n);
-		n++;
 	}
-	while (c <= 'f')
+	for (c = 'a'; c <= 'f' ; c++)
 	{
 		putchar(c);
-		c++;
 	}
 	putchar('\n');
 	return (0);
