3c90f40a0acf2f5ba0ea4f12b856a66e8048b295
Salman I <sal.idris27@gmail.com>
2022-03-16 15:13:34 -0700

    Function that adds two int and print their sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns result
+ * @x: operand 1
+ * @y: operand 2
+ * Return: sum
+ */
+int add(int x, int y)
+{
+	int r;
+
+	r = x + y;
+
+	return (r);
+}
