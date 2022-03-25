ba257c8a12c8e2ebc5e30165dedca429f4e19791
Salman I <sal.idris27@gmail.com>
2022-03-25 16:38:19 -0700

    Fix function to print diagonal line in terminal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,14 +20,14 @@ void print_diagonal(int n)
 		{
 			for (j = 0; j < n; j++)
 			{
-					if (j == 1)
-					{
-						_putchar('\\');
-					}
-					else if (j < i)
-					{
-						_putchar(' ');
-					}
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else if (j < i)
+				{
+					_putchar(' ');
+				}
 			}
 			_putchar('\n');
 		}
