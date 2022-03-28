a6484202a898c39641ad8f7b2a2f4bb9e129a884
Salman I <sal.idris27@gmail.com>
2022-03-28 12:52:21 -0700

    FFunction that copies a memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area source
+ * to destination
+ * @dest: where to get copied to
+ * @src: where it  is copied from
+ * @n: bytes to be copied
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
